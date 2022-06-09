ae684852e53be0060075bb263f89ee690a495839
unknown <sergiogermanpagano@gmail.com>
2022-06-09 16:23:12 -0300

    update1

---
+++ src/componentes/testimonio.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function testimonios(){
+  return(
+          <div className="contenedor-testimonio">
+              <img
+                  className="imagen-testimonio" src="../imagenes/testimonio-emma.png" alt="" />
+
+
+
+          </div>
+
+  );
+
+};
