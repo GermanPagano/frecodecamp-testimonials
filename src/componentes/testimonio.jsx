19ee72a7281628d33dbbd9f5b83d25ef92f92898
unknown <sergiogermanpagano@gmail.com>
2022-06-10 15:15:45 -0300

    subiendo detalles

--- src/componentes/testimonio.jsx
+++ src/componentes/testimonio.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import "../stylesheets/testimonios.css"
 
 
 export
-function Testimonio(){
+function Testimonio(props){
   return(
           <div className="contenedor-testimonio">
               <img
-                className="imagen-testimonio" src={require("../imagenes/testimonio-emma.png")} alt="Fotografia de emma" />
+                className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="Fotografia de emma" />
 
               <div className="contenedor-texto-testimonio">
-                  <p className="nombre-testimonio">Emma Bostian en Sweden</p>
-                  <p className="cargo-testimonio">Ingeniera de Software en Spotify </p>
-                  <p className="text-testimonio">"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y
-                  la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+
+                  <p className="nombre-testimonio">
+                  <strong>{props.nombre}</strong> in {props.pais}</p>
+
+                  <p className="cargo-testimonio">
+                    {props.cargo} at <strong>{props.empresa}</strong> </p>
+
+                  <p className="text-testimonioSiem">
+                    "{props.testimonio}"</p>
               </div>
 
           </div>
