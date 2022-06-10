c715e07bfc19300510ec9b8ae543b1d29a6981d2
unknown <sergiogermanpagano@gmail.com>
2022-06-10 01:14:40 -0300

    estilos 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
 
     <div className="App">
+      <div className="contenedor-principal">     
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
       <Testimonio/>
+      </div>
     </div>
   );
 }
